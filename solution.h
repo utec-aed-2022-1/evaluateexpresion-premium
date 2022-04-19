7800c901627a594149f7ba27d6c8b6db1f7d7d88
Alessandro <giuffra.alessandro@gmail.com>
2022-04-19 18:47:22 -0500

    Se realizó el algoritmo para lograr 5/7 pruebas, falta evaluar valores de dos dígitos

--- solution.h
+++ solution.h
@@ -7,161 +7,144 @@
 
 using namespace std;
 
-template <typename T>
-struct Node
-{
-    T data;
-    Node<T> *next;
-
-    Node()
-    {
-        this->data = NULL;
-        this->next = nullptr;
-    }
-
-    Node(T value)
-    {
-        this->data = value;
-        this->next = nullptr;
-    }
-
-    void killSelf()
-    {
-        delete this;
-    }
-};
-
-template <typename T>
-class List
-{
-private:
-    Node<T> *head;
-    int nodes;
-
-public:
-    List()
-    {
-        head = nullptr;
-        nodes = 0;
-    }
-
-    virtual ~List()
-    {
-        delete this;
-    }
-
-    void push_front(T data)
-    {
-        Node<T> *nuevo = new Node<T>(data);
-        nuevo->next = head;
-        head = nuevo;
-        ++nodes;
-    }
-
-    T pop_front()
-    {
-        if (head == nullptr)
-            throw("No hay datos");
-        Node<T> *temp = head;
-        head = head->next;
-        T val = temp->data;
-        delete temp;
-        --nodes;
-        return val;
-    }
-
-    void display()
-    {
-        Node<T> *temp = head;
-        while (temp != nullptr)
-        {
-            cout << temp->data << " ";
-            temp = temp->next;
-        }
-        cout << endl;
-    }
-
-    int str2int()
-    {
-        return 0;
-    }
-};
-
 struct Result
 {
     double result;
     bool error;
 };
 
-void removeSpaces(char *s)
+float doMath(char op, int op1, int op2)
 {
-    *std::remove(s, s + strlen(s), ' ') = 0;
+    switch (op) 
+    {
+        case '+':
+            return (float)op1 + (float)op2;
+        case '-':
+            return (float)op1 - (float)op2;
+        case '*':
+            return (float)op1 * (float)op2;
+        case '/':
+            return (float)op1 / (float)op2;
+    }
 }
 
 Result evaluate(string input)
 {
     std::map<char, int> prec = {
-            { '*', 3 },
-            { '/', 3 },
-            { '+', 2 },
-            { '-', 2 },
-            { '(', 1 }
+            { '*', 2 },
+            { '/', 2 },
+            { '+', 1 },
+            { '-', 1 }
     };
 
     Result resultado;
     // 1
-    stack<char> *opstack; // Lista para operadores
-    list<char> postfixList;
+    stack<char> *opstack = new stack<char>; // pila para operadores
+    list<char> postfixList; // lista para el orden postfijo
     // 2
 
     // Creamos una lista para los caracteres
-    list<char> tokenList = new list<char>;
-    remove(input.begin(), input.end(), ' ');
+    list<char> *tokenList = new list<char>;
+    // remove(input.begin(), input.end(), ' ');
     for (int i = 0; i < input.size(); i++)
-        tokenList.push_front(input[i]);
+        tokenList->push_back(input[i]);
 
-    // 3
-    list<char> tokenList;
     // crear lista de números (operandos)
-    list<char> numlist = ['1','2','3','4','5','6','7','8','9','0'];
+    list<char> *numlist = new list<char>;
+    numlist->push_front('0');
+    numlist->push_front('1');
+    numlist->push_front('2');
+    numlist->push_front('3');
+    numlist->push_front('4');
+    numlist->push_front('5');
+    numlist->push_front('6');
+    numlist->push_front('7');
+    numlist->push_front('8');
+    numlist->push_front('9');
 
-
-    for(auto token = tokenList.begin(); token!= tokenList.end(); ++token) {
+    // FOR que recorre el input para ser analizado
+    for(auto token = tokenList->begin(); token!= tokenList->end(); ++token) {
         // Caso token es operando
-        for (auto num = numlist.begin(); num != numlist.end(); ++num)
+        for (auto num = numlist->begin(); num != numlist->end(); ++num)
             if (*num == *token)
-                postfixList.pushback(*token);
+                postfixList.push_back(*token);
         // Caso token es (
-        if (token == "(")
-            opstack.push(*token);
+        if (*token == '(')
+            opstack->push(*token);
         // Caso token es )
-        if (token == ")")
+        if (*token == ')')
+        {
             // Obtener último elemento agregado al stack
-            char topToken = opstack.top();
-        opstack.pop(); // Elimina el último elemento del stack
-        // Mientras el siguiente elemento no sea apertura '(',
-        //              Agrega todos los operadores a la lista de salida postfixList
-        while (topToken != '(') {
-            postfixList.push(topToken);
-            topToken = opstack.top;
-            opstack.pop();
+            char topToken = opstack->top();
+            opstack->pop(); // Elimina el último elemento del stack
+            // Mientras el siguiente elemento no sea apertura '(',
+            // Agrega todos los operadores a la lista de salida postfixList
+            while (topToken != '(')
+            {
+                postfixList.push_back(topToken);
+                topToken = opstack->top();
+                opstack->pop();
+            }
         }
+        // Caso sea operador
+        if (prec.count(*token))
+        {
+            while (!(opstack->empty()) && (prec.find(opstack->top())->second >= prec.find(*token)->second))
+            {
+                postfixList.push_back(opstack->top());
+                opstack->pop();
+            }
+            opstack->push(*token);
+        }
+    }
+
+    while (!opstack->empty()){
+        char temp = opstack->top();
+        opstack->pop();
+        postfixList.push_back(temp);
+    }
+
+    stack<float> *restack = new stack<float>;
+    float result = 0.0;
+
+    while (!postfixList.empty())
+    {
+        // Si hay error
+        if (postfixList.front() == '(')
+        {
+            resultado.error = true;
+            return resultado;
+        }
+        // Si es operando
+        if (!prec.count(postfixList.front()))
+        {
+            restack->push((float)postfixList.front() - '0');
+        }
+        // si es operador
         else
-        while (!opstack.empty() && (prec[opstack.top()] >= prec[token])) {
-            postfixList.pushback(opstack.top());
-            opstack.pop();
-            opstack.push(token);
+        {
+            if (restack->empty())
+            {
+                resultado.error = true;
+                return resultado;
+            }
+            float operand2 = restack->top();
+            restack->pop();
+            if (restack->empty())
+            {
+                resultado.error = true;
+                return resultado;
+            }
+            float operand1 = restack->top();
+            restack->pop();
+
+            result = doMath(postfixList.front(), operand1, operand2);
+            restack->push(result);
         }
+        postfixList.pop_front();
     }
 
-    while (!opstack.empty()){
-        postfixList.pushback(opstack.pop())
-    }
-
-
-
-    // +
-    // 5 8
+    resultado.result = result;
 
     // 1- descomponer el input y validar
 
@@ -171,5 +154,5 @@ Result evaluate(string input)
 
     //* Si no cumple la validacion retornar Result.error = true;
 
-    return Result();
+    return resultado;
 }
